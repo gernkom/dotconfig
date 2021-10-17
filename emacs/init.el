ec7d925752d057860504aaddddae45f896fdd62f
Gernkom Tsang <gernkom7@gmail.com>
2021-10-17 19:30:23 +0800

    删除无用的配置

--- emacs/init.el
+++ emacs/init.el
@@ -59,17 +59,3 @@
   :diminish
   :init (add-hook 'after-init-hook 'yas-global-mode))
 (use-package yasnippet-snippets)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(yasnippet-snippets yasnippet company which-key monokai-theme all-the-icons use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
