fc4117698376f9649ade7770f36409f631247e4b
Gernkom Tsang <gernkom7@gmail.com>
2021-12-04 18:36:37 +0000

    更新了一些东西

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", "15", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *volup[] = { "amixer", "set", "Master", "5%+", "umute", NULL };
 static const char *voldown[] = { "amixer", "set", "Master", "5%-", "umute", NULL };
