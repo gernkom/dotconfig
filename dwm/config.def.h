87e1dd769c0956493ec615a5fe0b8d7dbe030cfe
Gernkom Tsang <gernkom7@gmail.com>
2021-12-07 22:47:43 +0000

    更新了一些东西

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -37,8 +37,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Chromium", NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Telegram", NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
