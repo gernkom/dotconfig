a146d341afa745a3f1e561c788d522b5f1765772
Gernkom Tsang <gernkom7@gmail.com>
2022-03-06 03:52:55 +0800

    优化字体大小，定义 mpv 打开规则

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "Sarasa Term SC:size=15" };
-static const char dmenufont[]       = "Sarasa Term SC:size=15";
+static const char *fonts[]          = { "Sarasa Term SC:size=13" };
+static const char dmenufont[]       = "Sarasa Term SC:size=13";
 static const char col_gray1[]       = "#1D2330";
 static const char col_gray2[]       = "#4B5665";
 static const char col_gray3[]       = "#54478A";
@@ -35,6 +35,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Telegram", NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "mpv",      NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
